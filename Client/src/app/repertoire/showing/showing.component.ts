6e62b0bc4c33d2af4e49df9b5ccc7fbc04cfecb4
Dorota Stys <stysdor@gmail.com>
2021-06-11 15:56:42 +0200

    Adding styling to ShowingComponent

--- Client/src/app/repertoire/showing/showing.component.ts
+++ Client/src/app/repertoire/showing/showing.component.ts
@@ -8,9 +8,15 @@ import { IShowing } from '../../shared/models/showing';
 })
 export class ShowingComponent implements OnInit {
   @Input() showing: IShowing;
+
+  day: string;
+  hour: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.day = `${String(this.showing.showingDateTime.getDate()).padStart(2, "0")}.${String(this.showing.showingDateTime.getMonth()).padStart(2, "0")}`;
+    this.hour = `${this.showing.showingDateTime.getHours()}:${String(this.showing.showingDateTime.getMinutes()).padStart(2, "0")}`;
   }
 
 }
